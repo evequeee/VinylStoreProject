87f796fae9b3b5fdcc4532ec1c936d89f9b650b8
evequeee <bogdankors1@gmail.com>
2025-06-29 18:24:40 +0300

    finished working on specified repos

---
+++ ProjectVinylStore.DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectVinylStore.DataAccess.Entities;
+using ProjectVinylStore.DataAccess.Interfaces;
+using System.Collections.Generic;
+
+namespace ProjectVinylStore.DataAccess.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserIdAsync(int userId)
+        {
+            return await _context.Orders
+                .Where(o => o.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Order>> GetRecentOrdersAsync(int count)
+        {
+            return await _context.Orders
+                .OrderByDescending(o => o.OrderDate)
+                .Take(count)
+                .ToListAsync();
+        }
+
+        public async Task<decimal> GetTotalSalesAsync(DateTime startDate, DateTime endDate)
+        {
+            return await _context.Orders
+                .Where(o => o.OrderDate >= startDate && o.OrderDate <= endDate)
+                .SumAsync(o => o.TotalAmount);
+
+        }
+    }
+}
---
+++ ProjectVinylStore.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectVinylStore.DataAccess.Entities;
+using ProjectVinylStore.DataAccess.Interfaces;
+
+namespace ProjectVinylStore.DataAccess.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User> GetUserWithOrdersAsync(int userId)
+        {
+            return await _context.Users
+                .Include(u => u.Orders)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+    }
+}
